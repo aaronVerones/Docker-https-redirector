3629c302c0f15c27100371eefcf2b7a572069030
Aaron Verones <aaron.verones@alumni.ubc.ca>
2018-06-20 19:01:38 -0700

    change debug message to say "redirector listening"

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const port = 80
 // don't listen on localhost or any routes under /insecure
 app.use(https([/localhost(:\d{1,5})?/], [/\/insecure/], 301));
 app.listen(port);
-debug(`redirector listening on port ${port}`);
+debug(`redirector listening`);
 module.exports = app;
